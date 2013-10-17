d75138f1289f5655a871939d968515594f93e3f3
Balint Erdi <balint.erdi@gmail.com>
2013-10-17 15:39:33 +0200

    Modify comp for Episode 6 that shows the two-step song creation process

--- comps/js/main.js
+++ comps/js/main.js
@@ -2,4 +2,8 @@
   $('.rating-star').on('mouseover', function() {
     console.log('Rate: ', $(this).attr('data-rating'));
   });
+  $('#create-song-link').on('click', function() {
+    $('.empty-list').hide();
+    $('#new-song-panel').show();
+  });
 })(jQuery)
