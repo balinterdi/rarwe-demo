7ce4f86c9ece5e8730f0a1b97a5a490f9cbd1b2f
Balint Erdi <balint.erdi@gmail.com>
2016-02-25 08:24:03 +0100

    Use active-model-adapter as the application's adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,6 @@
-import DS from 'ember-data';
+import ActiveModelAdapter from 'active-model-adapter';
 
-export default DS.RESTAdapter.extend({
+export default ActiveModelAdapter.extend({
   namespace: 'api',
   shouldBackgroundReloadRecord() {
     return false;
