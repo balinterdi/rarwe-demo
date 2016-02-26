359102db65dec508fe0105aa8a0ab73ffa32de1a
Balint Erdi <balint.erdi@gmail.com>
2016-02-26 10:25:55 +0100

    Remove commented out fixtures

--- mirage/fixtures/bands.js
+++ mirage/fixtures/bands.js
@@ -1,8 +1,4 @@
 export default [
-  // { id: 1, name: 'Pearl Jam', songIds: [1, 2, 3, 4, 5] },
-  // { id: 2, name: 'Led Zeppelin', songIds: [6, 7, 8] },
-  // { id: 3, name: 'Foo Fighters', songIds: [9] },
-  // { id: 4, name: 'Radiohead', songIds: [] }
   { id: 1, name: 'Pearl Jam', links: { songs: '/api/bands/1/songs' } },
   { id: 2, name: 'Led Zeppelin', links: { songs: '/api/bands/2/songs' } },
   { id: 3, name: 'Foo Fighters', links: { songs: '/api/bands/3/songs' } },
--- mirage/fixtures/songs.js
+++ mirage/fixtures/songs.js
@@ -1,13 +1,4 @@
 export default [
-  // { id: 1, title: 'Daughter', rating: 5 },
-  // { id: 2, title: 'Yellow Ledbetter', rating: 5 },
-  // { id: 3, title: 'Animal', rating: 4 },
-  // { id: 4, title: 'Inside Job', rating: 4 },
-  // { id: 5, title: 'Who We Are', rating: 2 },
-  // { id: 6, title: 'Black Dog', rating: 4 },
-  // { id: 7, title: 'Achilles Last Stand', rating: 5},
-  // { id: 8, title: 'Immigrant Song', rating: 4},
-  // { id: 9, title: 'Pretender', rating: 3 }
   { id: 1, title: 'Daughter', rating: 5, bandId: 1 },
   { id: 2, title: 'Yellow Ledbetter', rating: 5, bandId: 1 },
   { id: 3, title: 'Animal', rating: 4, bandId: 1 },
