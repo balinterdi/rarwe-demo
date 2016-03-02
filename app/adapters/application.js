179dcf1e975d07a514b7249e0687b960eabfeaf1
Balint Erdi <balint.erdi@gmail.com>
2016-03-02 09:30:22 +0100

    Allow reloading band records in the background

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,9 +2,6 @@ import ActiveModelAdapter from 'active-model-adapter';
 
 export default ActiveModelAdapter.extend({
   namespace: 'api',
-  shouldBackgroundReloadRecord() {
-    return false;
-  },
 });
 
 // API lived at http://rock-and-roll-with-emberjs-api.herokuapp.com
