3a64277b958c5359db3195b4e3914ee1367bb69e
Balint Erdi <balint.erdi@gmail.com>
2016-10-12 09:40:25 +0200

    Add acceptance test for displaying songs

---
+++ tests/acceptance/songs-test.js
@@ -0,0 +1,33 @@
+import Ember from 'ember';
+import { test } from 'qunit';
+import moduleForAcceptance from 'rarwe-demo/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | Songs', {
+  beforeEach() {
+    let tool = server.create('band', {
+      id: 1,
+      name: 'Tool',
+      links: { songs: '/api/bands/1/songs' }
+    });
+    server.create('band', {
+      id: 2,
+      name: 'Pearl Jam',
+      links: { songs: '/api/bands/2/songs' }
+    });
+
+    server.create('song', { title: '46 & 2', bandId: tool.id });
+    server.create('song', { title: 'Lateralus', bandId: tool.id });
+    server.create('song', { title: 'Parabola', bandId: tool.id });
+  }
+});
+
+test('List songs for a band', function(assert) {
+  server.logging = true;
+  visit('/bands');
+  click('a:contains("Tool")');
+
+  andThen(function() {
+    assert.equal(Ember.$('.song').length, 3, "All songs for the selected band are displayed");
+    assert.equal(Ember.$('.song:contains("46 & 2")').length, 1, "The first song is displayed");
+  });
+});
